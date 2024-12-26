8cbc7a73376a03cfa5e6436a2cdd97fa7be7dc2c
David <davidagamez04@gmail.com>
2024-12-26 16:14:55 -0500

    feat: add CommentsModule to AppModule for comment management functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { TeamsModule } from './teams/teams.module';
+import { CommentsModule } from './comments/comments.module';
 @Module({
-	imports: [AuthModule, ProjectsModule, TasksModule, TeamsModule],
+	imports: [AuthModule, ProjectsModule, TasksModule, TeamsModule, CommentsModule],
 })
 export class AppModule {}
