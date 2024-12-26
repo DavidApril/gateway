0d1922b4fb8fdce9f7a781c282d1b4c7e587cbd6
David <davidagamez04@gmail.com>
2024-12-26 16:11:00 -0500

    feat: enhance LoginUserDto with Swagger API properties for improved documentation and validation

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+  @ApiProperty({ example: 'user@example.com', description: 'The email of the user' })
   @IsEmail()
   @IsString()
   email: string;
 
+  @ApiProperty({ example: 'Password#2023', description: 'The strong password of the user' })
   @IsString()
   @IsStrongPassword()
   password: string;
