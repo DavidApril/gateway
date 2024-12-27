c4e8dfd2666a290add5c73a2b6e63979a165aea7
David <davidagamez04@gmail.com>
2024-12-27 00:49:35 -0500

    feat: export new DTOs for associating users and projects in team management

--- src/teams/dto/index.ts
+++ src/teams/dto/index.ts
@@ -1,2 +1,5 @@
+export * from './associate-to-project.dto';
+export * from './associate-user.dto';
 export * from './create-team.dto';
+export * from './team-pagination.dto';
 export * from './update-team.dto';
