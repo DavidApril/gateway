c6eedec78095b9d3bfa8d93c0364d9dd54700c28
David <davidagamez04@gmail.com>
2024-12-27 00:48:51 -0500

    feat: enhance CreateTeamDto with Swagger documentation for team name and description

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -1,23 +1,15 @@
-import { IsArray, IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateTeamDto {
+	@ApiProperty({ description: 'The name of the team', example: 'Inlaze' })
 	@IsString()
 	@IsNotEmpty()
 	name: string;
 
+	@ApiProperty({ description: 'A brief description of the team', example: 'Develop team of Inlaze' })
 	@IsString()
 	@IsNotEmpty()
 	description: string;
-
-	@IsUUID()
-	@IsNotEmpty()
-	project_id: string;
-
-	@IsUUID()
-	@IsNotEmpty()
-	owner: string;
-
-	@IsArray()
-	@IsOptional()
-	members: string[];
 }
