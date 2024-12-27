1e45ac6a70569acf4eef72f2aea4fa9f8f591346
David <davidagamez04@gmail.com>
2024-12-27 00:48:39 -0500

    feat: enhance UpdateTeamDto with Swagger documentation for team identifier

--- src/teams/dto/update-team.dto.ts
+++ src/teams/dto/update-team.dto.ts
@@ -1,8 +1,9 @@
 import { CreateTeamDto } from 'src/projects/dto';
 import { IsUUID } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class UpdateTeamDto extends PartialType(CreateTeamDto) {
 	@IsUUID()
+	@ApiProperty({ description: 'The unique identifier for the team' }) // Add Swagger documentation
 	id: string;
 }
