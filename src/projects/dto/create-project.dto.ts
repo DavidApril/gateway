1f5f102c835d0cd18a8bb037fcb2a8f96b54a705
David <davidagamez04@gmail.com>
2024-12-27 00:48:22 -0500

    feat: enhance CreateProjectDto with example values for improved API documentation

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -2,17 +2,17 @@ import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProjectDto {
-	@ApiProperty({ description: 'The name of the project' })
+	@ApiProperty({ description: 'The name of the project', example: 'Task Management App' })
 	@IsString()
 	@IsNotEmpty()
 	name: string;
 
-	@ApiProperty({ description: 'The description of the project' })
+	@ApiProperty({ description: 'The description of the project', example: 'Application for projects management' })
 	@IsString()
 	@IsNotEmpty()
 	description: string;
 
-	@ApiProperty({ description: 'The UUID of the project owner' })
+	@ApiProperty({ description: 'The UUID of the project owner', example: 'c8661452-f705-49ee-a4b7-d09a5d586ee4' })
 	@IsString()
 	@IsNotEmpty()
 	@IsUUID()
